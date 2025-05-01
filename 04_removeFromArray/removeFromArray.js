2e801b0699928d0c5222155e667fd9e3f6f2c821
Yojhan <sryojhan@gmail.com>
2025-05-01 04:18:57 +0200

    exercise 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elem) {
+
+    return arr.filter((value) => {
+
+        return !elem.some((element) =>{
+
+            return value === element;
+        });
+    });
 };
 
 // Do not edit below this line
