3eff5df69266317926e8611f2548c2831f317d13
Yojhan <sryojhan@gmail.com>
2025-05-01 18:53:04 +0200

    exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(begining, end) {
+
+    if(typeof begining !== 'number' || typeof end !== 'number')
+        return "ERROR";
+
+
+    if(end < begining)
+    {
+        let temp = begining;
+        begining = end;
+        end = temp;
+    }
+
+    if(begining < 0 || begining % 1 !== 0 || end % 1 !== 0)
+    {
+        return "ERROR";
+    }
+
+    let counter = 0;
+
+    for(let i = begining; i <= end; i++){
+
+        counter += i;
+    }
+
+
+    return counter;
 
 };
 
