f6c5be6f9765b230e5eddd9c1a4c210662d61621
Yojhan <sryojhan@gmail.com>
2025-05-01 03:29:31 +0200

    exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
 
+    if(count < 0) return "ERROR";
+
+    let str = "";
+
+    for(let i = 0; i < count; i++)
+        str += string;
+
+    return str;
 };
 
 // Do not edit below this line
