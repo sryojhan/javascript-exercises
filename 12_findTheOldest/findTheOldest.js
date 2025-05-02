33c865572944e127546c7ab4e64b3b859c249d60
Yojhan <sryojhan@gmail.com>
2025-05-03 00:08:50 +0200

    exercise 12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    function calculateAge(current)
+    {
+        if("yearOfDeath" in current === false)
+            current.yearOfDeath = 2025;
+
+        return current.yearOfDeath - current.yearOfBirth;
+
+    }
+
+
+    return people.reduce((previous, current)=>{
+
+        if(calculateAge(current) > calculateAge(previous))
+            return current;
+        return previous;
+    });
 };
 
 // Do not edit below this line
